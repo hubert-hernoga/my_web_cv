523a75a3ead479e5419da08210a0434f0b8bd30c
wezymord <hubert.hernoga@wp.pl>
2018-04-15 22:40:27 +0200

    Reset changes

--- CV/urls.py
+++ CV/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from cv_maker.views import TitlePage, CoverLatter, Projects, ProjectShop, ProjectCV, Portfolio, GeneratorPDF
+from cv_maker.views import TitlePage, CoverLatter, Projects, ProjectShop, ProjectCV, Portfolio
 
 
 urlpatterns = [
